7d42ef3241ff48bbd5bad1abfd827e5b457fec81
Marco Mello <marmello@usp.br>
2021-09-29 18:26:47 -0300

    New tutorial
    Added a tutorial in RMD to help reproduce the case study. Also reviewed the respective R file.

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
 # nestedness
-Commented scripts to reproduce our study and R functions for applying the proposed procedures on alternative networks
 
-Readme in file: "readme.pdf" 
-https://github.com/pinheirorbp/nestedness/blob/master/readme.pdf
+Supplementary code and data from the study: Pinheiro, R. B. P., Dormann, C. F., Felix, G. M. F., & Mello M. A. R. **Linking null models to hypotheses to improve nestedness analysis**. *Submitted*.
+
+[Ecological Synthesis Lab](https://marcomellolab.wordpress.com) (SintECO)
+
+Authors: Rafael B. P. Pinheiro, Gabriel M. Felix, Carsten F. Dormann, and Marco A. R. Mello.
+
+E-mail: [rafael-bpp\@hotmail.com](mailto:rafael-bpp@hotmail.com){.email}.
+
+Published originaly on September 29th, 2021 (English version).
+
+[**Add Zenodo badge here**]
+
+Run in R version 4.0.3 (2020-10-10) -- "Bunny-Wunnies Freak Out"
+
+## Disclaimer
+
+You may use this script freely for commercial or non-commercial purposes at your own risk. We assume no responsibility or liability for the use of this software, convey no license or title under any patent, copyright, or mask work right to the product. We reserve the right to make changes in the software without notification. We also make no representation or warranty that such application will be suitable for the specified use without further testing or modification. If this script helps you produce any academic work (paper, book, chapter, dissertation, monograph, thesis, report, talk, keynote, lecture etc.), please acknowledge the authors and cite the source.
+
+## General instructions
+
+In this repo you will find commented scripts to reproduce our study, including R functions for applying our new protocol to other networks.
+
+If you are well acquainted with R, you may reproduce the case study presented in our paper using the file `case_study.R`. You may also experiment with our custom-made functions in the folder `functions`.
+
+Alternatively, you can run the step-by-step tutorial provided in the file `case_study.Rmd`. Or just read its knitted PDF version in the file `case_study.pdf`.
+
+See further instructions and details about our protocol in the **complete user guide**: [README file](https://github.com/pinheirorbp/nestedness/blob/master/readme.pdf).
+
+## Related GitHub repos
+
+1.  <https://github.com/pinheirorbp>
+2.  <https://github.com/gabrielmfelix>
+3.  <https://github.com/biometry/bipartite>
+4.  <https://github.com/marmello77>
+
+## Acknowledgments
+
+We thank our institutions and colleagues, who helped us in different ways during this project. The Ph.D. defense committee of RBPP, composed by Tatiana Cornelissen, Aristóteles Neto, Mario Almeida-Neto, and Paulo Peixoto, provided suggestions that considerably improved the quality of our study. The Graduate School in Ecology of the Federal University of Minas Gerais (ECMVS), Brazil, provided RBPP with a Ph.D. scholarship from the Brazilian Council for Scientific and Technological Development (CNPq). Infrastructure for this study was provided by ECMVS, the Department of Ecology of the University of São Paulo (USP), Brazil, and the Department of Biometry and Environmental System Analysis of the University of Freiburg, Germany. The Graduate School in Ecology of the State University of Campinas (PPGE-UNICAMP), Brazil, provided GMFF with a scholarship from the Brazilian Coordination for the Improvement of Higher Education Personnel (CAPES). RBPP received a scholarship from the joint program between CAPES, CNPq, and Deutscher Akademischer Austauschdienst (DAAD) (88887.161398/2017-00) and is currently funded by São Paulo Research Foundation (FAPESP, post-doctoral grant 2020/06771-2). MARM was funded by the Minas Gerais Research Foundation (FAPEMIG: PPM-00324-15), Alexander von Humboldt Foundation (AvH, 3.4-8151/15037 and 3.2-BRA/1134644), CNPq (302700/2016-1 and 304498/2019-0), Dean of Research of the University of São Paulo (PRP-USP, 18.1.660.41.7), and FAPESP (2018/20695-7). Last, but not least, we thank the [StackOverflow](https://stackoverflow.com) community, where we solve most of our coding dilemmas.
--- case_study.R
+++ case_study.R
@@ -1,37 +1,130 @@
-### Authors ####
+################################################################################
 
-#Rafael Barros Pereira Pinheiro 
-#Carsten F. Dormann 
-#Gabriel Moreira Felix Ferreira
-#Marco Aurelio Ribeiro Mello 
+# Supplementary code and data from the study: Pinheiro, R. B. P., Dormann, 
+# C. F., Felix, G. M. F., & Mello M. A. R. Linking null models to hypotheses
+# to improve nestedness analysis. Submitted.
+
+################################################################################
+
+
+# Authors:
+
+# Rafael R. B. Pinheiro 
+# Carsten F. Dormann 
+# Gabriel M. Felix
+# Marco A. R. Mello 
 
 # Contact: rafael-bpp@hotmail.com
 
-### Case study ###
-# The empirical network analyzed was downloaded from the Web of Life database (www.web-of-life.es, network: M_PL_060_17).
-# Originally described by Kaiser-Bunbury 2010.
-#Kaiser-Bunbury CN, Muff S, Memmott J, M?ller CB, Caflisch A. 2010. The robustness of pollination networks to the loss of species and interactions: a quantitative approach incorporating pollinator behaviour. Ecol. Lett. 13, 442-452. (doi:10.1111/j.1461-0248.2009.01437.x).
-net1=as.matrix(read.table("data/net.txt"))
-### Requirements ####
+# Ecological Synthesis Lab (SintECO): https://marcomellolab.wordpress.com.
+
+# See README for the complete user guide to our new protocol:
+# https://github.com/pinheirorbp/nestedness/blob/master/readme.pdf
+
+# Disclaimer: You may use this script freely for commercial or non-commercial 
+# purposes at your own risk. We assume no responsibility or liability for the 
+# use of this software, convey no license or title under any patent, copyright,
+# or mask work right to the product. We reserve the right to make changes in 
+# the software without notification. We also make no representation or warranty
+# that such application will be suitable for the specified use without further
+# testing or modification. If this script helps you produce any academic work
+# (paper, book, chapter, dissertation, monograph, thesis, report, talk, keynote,
+# lecture, and similar), please acknowledge the authors and cite the source.
+
+
+##### CASE STUDY #####
+
+
+# Use the following code to reproduce the case study presented in our paper.
+# The file net.txt represents the network analyzed by Kaiser-Bunbury et al. 
+# (2010). If you want to apply our new protocol to your own data, just replace
+# net.txt with another network of your choice, following the same format.
+
+# The empirical network analyzed in our paper was downloaded from the Web of 
+# Life database (http://www.web-of-life.es, network: M_PL_060_17). It was
+# originally analyzed by: 
+
+# Kaiser-Bunbury CN, Muff S, Memmott J, M?ller CB, Caflisch A. 2010. The 
+# robustness of pollination networks to the loss of species and interactions: 
+# a quantitative approach incorporating pollinator behaviour. Ecol. Lett. 13,
+# 442-452. (http://dx.doi.org/10.1111/j.1461-0248.2009.01437.x).
+
+# Create a new object with the network to be analyzed.
+net1 <- as.matrix(read.table("data/net.txt"))
+
+# Load the required packages
 library(bipartite)
+
+# Load our custom-made function to calculate nestedness and run the null models.
+# Check out the comments in the respective R file to obtain detailed information
+# about the function, including its arguments and options.
 source("functions/nestmodels.R")
-### Plot of the matrix ####
-plotmatrix(net1, binary=F, within_color = "#0e0a44FF",base_color = "#d7d6e9ff")
-### WNODF ####
-WNODF=nestmodels(M=net1,index="wnodf",prop.model = T,n.model = 10000,calc.at = c(50,100,200,300,400,444), print.at.each = 100,density.plot = T,sampling.plot = T)
+
+# Plot the matrix to get a feeling about its structure.
+plotmatrix(net1, 
+           binary = F, 
+           within_color = "#0e0a44FF",
+           base_color = "#d7d6e9ff")
+
+# The simulations included in our protocol are quite resource-consuming and may
+# require several minutes, depending on your computer's processing power. 
+# Therefore, choose the number of randomized matrices (iterations) carefully.
+# In our paper, we ran 10,000 iterations, but we suggest experimenting with 
+# smaller numbers first.
+
+matrices <- 10000 #This number will be the same for all functions
+
+# Generate randomized matrices and calculate their degree of nestedness using
+# different metrics.
+
+# WNODF
+WNODF=nestmodels(M=net1,
+                 index="wnodf",
+                 n.model = matrices,
+                 prop.model = T,
+                 print.at.each = 100,
+                 density.plot = T,
+                 sampling.plot = T,
+                 calc.at = c(50,100,200,300,400,444))
 WNODF$observed
 WNODF$significance
-### WNODA ####
-WNODA=nestmodels(M=net1,index="wnoda",prop.model = T,n.model = 10000,print.at.each = 10,density.plot = T,sampling.plot = T,calc.at = c(50,100,200,300,400,444))
+
+# WNODA
+WNODA=nestmodels(M=net1,
+                 index="wnoda",
+                 n.model = matrices,
+                 prop.model = T,
+                 print.at.each = 100,
+                 density.plot = T,
+                 sampling.plot = T,
+                 calc.at = c(50,100,200,300,400,444))
 WNODA$observed
 WNODA$significance
-### NODF ####
+
+# NODF
 # Accounting for weighted marginal sums
-NODF1=nestmodels(M=net1,index="nodf",prop.model = T,n.model = 10000, print.at.each = 10,density.plot = T,equi.model = T,wprob = T,wsamp=F)
+NODF1=nestmodels(M=net1,
+                 index="nodf",
+                 n.model = matrices, 
+                 prop.model = T,
+                 equi.model = T,
+                 print.at.each = 100,
+                 density.plot = T,
+                 wprob = T,
+                 wsamp=F)
 NODF1$observed
 NODF1$significance
-# Only binary information
-NODF2=nestmodels(M=net1,index="nodf",prop.model = T,n.model = 10000, print.at.each = 10,density.plot = T,equi.model = T,wprob = F,wsamp=F)
+
+# Using only binary information
+NODF2=nestmodels(M=net1,
+                 index="nodf",
+                 n.model = matrices, 
+                 prop.model = T,
+                 equi.model = T,
+                 print.at.each = 100,
+                 density.plot = T,
+                 wprob = F,
+                 wsamp=F)
 NODF2$observed
 NODF2$significance
 
